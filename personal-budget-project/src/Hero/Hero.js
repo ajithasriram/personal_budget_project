aa3cebfa6cca2669058b9c31b1c48ef858129e71
Aparajitha Sriram <33689863+ajithasriram@users.noreply.github.com>
2020-12-07 22:09:06 -0500

    created personal-budget react components

---
+++ personal-budget-project/src/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero" id="title" role="banner" aria-roledescription="Page title">
+            <h1>Personal Budget</h1>
+            <h2>A personal-budget management app</h2>
+            {/* <div className="hero">
+              <img src="images/bg.png" alt="A person counting cash" width="400px" height="350px"/>
+            </div>             */}
+    </div>
+  );
+}
+
+export default Hero;

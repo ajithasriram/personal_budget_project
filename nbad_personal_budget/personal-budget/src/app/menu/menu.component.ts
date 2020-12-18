fde80b9ca4485afaa1559387459ff062b8790729
Aparajitha Sriram <33689863+ajithasriram@users.noreply.github.com>
2020-12-18 15:42:31 -0500

    front end added along with server side

---
+++ nbad_personal_budget/personal-budget/src/app/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pb-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logout() {
+    document.cookie = 'token = ';
+    window.location.href = '/login';
+  }
+}
